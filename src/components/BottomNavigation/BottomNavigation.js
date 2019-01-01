5c50af222de35b65079b551f104989530d2790c3
Earle-Poole <eepoole4@gmail.com>
2019-01-01 14:59:33 -0600

    minor changes

--- src/components/BottomNavigation/BottomNavigation.js
+++ src/components/BottomNavigation/BottomNavigation.js
@@ -5,10 +5,10 @@ const BottomNavigation = ({onRouteChange}) => {
     return(
         <div>
             <nav className='bb bt mw8 center mt4 mb2'>
-                <p onClick={() => onRouteChange('home')} className='link b pointer underline pa3 ph4-l f7 grow f6-l'>Home</p>
-                <p onClick={() => onRouteChange('contactus')} className='link b pointer underline pa3 ph4-l f7 grow f6-l'>Contact us</p>
-                <p onClick={() => onRouteChange('termsandconditions')} className='link b pointer underline pa3 ph4-l f7 grow f6-l'>Terms & Conditions</p>
-                <p onClick={() => onRouteChange('blog')} className='link b pointer underline pa3 ph4-l f7 grow f6-l'>Blog</p>
+                <p onClick={() => onRouteChange('home')} className='link b pointer underline bg-animate hover-bg-black-10 pa3 ph4-l f7 f6-l'>Home</p>
+                <p onClick={() => onRouteChange('contactus')} className='link b pointer underline bg-animate hover-bg-black-10 pa3 ph4-l f7 f6-l'>Contact us</p>
+                <p onClick={() => onRouteChange('termsandconditions')} className='link b pointer underline bg-animate hover-bg-black-10 pa3 ph4-l f7 f6-l'>Terms & Conditions</p>
+                <p onClick={() => onRouteChange('blog')} className='link b pointer underline bg-animate hover-bg-black-10 pa3 ph4-l f7 f6-l'>Blog</p>
             </nav>
             <div className='ma3 f7 f6-l dark-gray i'>
                 <p>Copyright © 2018 CMA Wallets, LLC</p>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,13 +4,13 @@ import './Navigation.css'
 const Navigation = ({onRouteChange}) => {
     return(
         <nav className='bb bt mw8 center mb4'>
-            <p onClick={() => onRouteChange('home')} className='link grow pointer underline pa3 ph4-l b f7 f6-l'>Home</p>
-            <p onClick={() => onRouteChange('leomilitary')} className='link grow pointer underline pa3 ph4-l b f7 f6-l'>LEO/Military</p>
-            <p onClick={() => onRouteChange('safetyuse')} className='link grow pointer underline pa3 ph4-l b f7 f6-l'>Safety & Use</p>
-            <p onClick={() => onRouteChange('wheretobuy')} className='link grow pointer underline pa3 ph4-l b f7 f6-l'>Where to buy</p>
-            <p onClick={() => onRouteChange('formslinks')} className='link grow pointer underline pa3 ph4-l b f7 f6-l'>Forms & Links</p>
-            <p onClick={() => onRouteChange('scenarios')} className='link grow pointer underline pa3 ph4-l b f7 f6-l'>Scenarios</p>
-            <p onClick={() => onRouteChange('productdesign')} className='link grow pointer underline pa3 ph4-l b f7 f6-l'>Product Design</p>
+            <p onClick={() => onRouteChange('home')} className='link pointer dib underline bg-animate hover-bg-black-10 pa3 ph4-l b f7 f6-l'>Home</p>
+            <p onClick={() => onRouteChange('leomilitary')} className='link pointer dib underline bg-animate hover-bg-black-10 pa3 ph4-l b f7 f6-l'>LEO/Military</p>
+            <p onClick={() => onRouteChange('safetyuse')} className='link pointer dib underline bg-animate hover-bg-black-10 pa3 ph4-l b f7 f6-l'>Safety & Use</p>
+            <p onClick={() => onRouteChange('wheretobuy')} className='link pointer dib underline bg-animate hover-bg-black-10 pa3 ph4-l b f7 f6-l'>Where to buy</p>
+            <p onClick={() => onRouteChange('formslinks')} className='link pointer dib underline bg-animate hover-bg-black-10 pa3 ph4-l b f7 f6-l'>Forms & Links</p>
+            <p onClick={() => onRouteChange('scenarios')} className='link pointer dib underline bg-animate hover-bg-black-10 pa3 ph4-l b f7 f6-l'>Scenarios</p>
+            <p onClick={() => onRouteChange('productdesign')} className='link pointer dib underline bg-animate hover-bg-black-10 pa3 ph4-l b f7 f6-l'>Product Design</p>
         </nav>
     )
 }
