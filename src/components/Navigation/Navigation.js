4f30caaf02b464a329ce2284e58a5792cb909b5c
Earle-Poole <eepoole4@gmail.com>
2019-01-21 15:14:37 -0600

    had to remove ESLint for Heroku

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,18 +1,20 @@
-import './Navigation.css';
 import React from 'react';
+import './Navigation.css'
 
-const Navigation = ({ onRouteChange }) => {
-	return (
-		<div id="navigation-bg">
-			<nav className="bb bt mw8 mb4 center flex-wrap">
-				<p onClick={() => onRouteChange("home")} className="link pointer underline bg-animate hover-bg-black-10 pa1 pa2-m pa3-l ma1 b f7 f6-m f5-l">Home</p>
-				<p onClick={() => onRouteChange("leomilitary")} className="link pointer underline bg-animate hover-bg-black-10 pa1 pa2-m pa3-l ma1 b f7 f6-m f5-l">LEO/Military</p>
-				<p onClick={() => onRouteChange("safetyuse")} className="link pointer underline bg-animate hover-bg-black-10 pa1 pa2-m pa3-l ma1 b f7 f6-m f5-l">Safety & Use</p>
-				<p onClick={() => onRouteChange("wheretobuy")} className="link pointer underline bg-animate hover-bg-black-10 pa1 pa2-m pa3-l ma1 b f7 f6-m f5-l">Where to buy</p>
-				<p onClick={() => onRouteChange("formslinks")} className="link pointer underline bg-animate hover-bg-black-10 pa1 pa2-m pa3-l ma1 b f7 f6-m f5-l">Forms & Links</p>
-				<p onClick={() => onRouteChange("scenarios")} className="link pointer underline bg-animate hover-bg-black-10 pa1 pa2-m pa3-l ma1 b f7 f6-m f5-l">Scenarios</p>
-				<p onClick={() => onRouteChange("productdesign")} className="link pointer underline bg-animate hover-bg-black-10 pa1 pa2-m pa3-l ma1 b f7 f6-m f5-l">Product Design</p>
-			</nav>
-		</div>
-	)
+const Navigation = ({onRouteChange}) => {
+    return(
+        <div id='navigation-bg'>
+            <nav className='bb bt mw8 mb4 center flex-wrap'>
+                <p onClick={() => onRouteChange('home')} className='link pointer underline bg-animate hover-bg-black-10 pa1 pa2-m pa3-l ma1 b f7 f6-m f5-l'>Home</p>
+                <p onClick={() => onRouteChange('leomilitary')} className='link pointer underline bg-animate hover-bg-black-10 pa1 pa2-m pa3-l ma1 b f7 f6-m f5-l'>LEO/Military</p>
+                <p onClick={() => onRouteChange('safetyuse')} className='link pointer underline bg-animate hover-bg-black-10 pa1 pa2-m pa3-l ma1 b f7 f6-m f5-l'>Safety & Use</p>
+                <p onClick={() => onRouteChange('wheretobuy')} className='link pointer underline bg-animate hover-bg-black-10 pa1 pa2-m pa3-l ma1 b f7 f6-m f5-l'>Where to buy</p>
+                <p onClick={() => onRouteChange('formslinks')} className='link pointer underline bg-animate hover-bg-black-10 pa1 pa2-m pa3-l ma1 b f7 f6-m f5-l'>Forms & Links</p>
+                <p onClick={() => onRouteChange('scenarios')} className='link pointer underline bg-animate hover-bg-black-10 pa1 pa2-m pa3-l ma1 b f7 f6-m f5-l'>Scenarios</p>
+                <p onClick={() => onRouteChange('productdesign')} className='link pointer underline bg-animate hover-bg-black-10 pa1 pa2-m pa3-l ma1 b f7 f6-m f5-l'>Product Design</p>
+            </nav>
+        </div>
+    )
 }
+
+export default Navigation;
